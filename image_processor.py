4831ba2d41388f87f8a606cbd6e854484725117b
Alex Banna <alexanderbanna@gmail.com>
2014-11-11 12:42:15 -0800

    blah

--- image_processor.py
+++ image_processor.py
@@ -153,9 +153,10 @@ def process_message(message, s3_output_bucket, s3_endpoint, job_id):
 			info_message("Downloading image from %s" % line)
 
 			try:
+
 				opt = "-P %s %s" % (output_dir, line)
 				info_message("downloading from %s" % line)
-				info_message("command: wget %s" % opt)
+				print "command: wget %s" % opt
 				return_code = call(["wget", opt], shell=True)
 				if return_code < 0:
 					info_message("wget exited with %s", return_code)
