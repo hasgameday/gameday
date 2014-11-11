a0bf80d55500c706e4eaf29cfaaf292b09955420
John Bender <johnb@hasoffers.com>
2014-11-11 12:40:54 -0800

    smairestnaosrietn

--- image_processor.py
+++ image_processor.py
@@ -175,7 +175,7 @@ def process_message(message, s3_output_bucket, s3_endpoint, job_id):
 
 		# Return the output url
 		return output_url
-	except IOError:
+	except:
 		error_message("An error occurred. Please show this to your class instructor.")
 		error_message(sys.exc_info()[0])
 		
